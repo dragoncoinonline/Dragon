1620bc993dfae7e674530934c0420740d8257b6c
Dragon Inc <32882725+dragoncoinonline@users.noreply.github.com>
2017-12-14 23:39:53 -0500

    Update DragonLockNew.sol

--- DragonLockNew.sol
+++ DragonLockNew.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 
 contract Dragon {
@@ -9,19 +9,82 @@ contract Dragon {
     
 }
 
+contract Ownable {
+  address public owner;
 
-contract DragonCrowdsale {
-    
-    function beneficiary() returns ( address );
-    
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
 
-contract DragonLock {
+
+
+contract DragonLock is Ownable {
     
-    address public owner;
+    using SafeMath for uint;
+   
+    address public dataentryclerk;
     Dragon public tokenreward; 
-    address public dragoncrowdsale;
+    
     
     mapping ( address => uint ) public dragonBalance;
    
@@ -29,18 +92,15 @@ contract DragonLock {
     uint public TimeLock;
  
     
-    
-    modifier onlyDragonCrowdsale () {
-        if (msg.sender != dragoncrowdsale ) {
+    modifier onlyOwner () {
+        if ( msg.sender != owner ) {
             throw;
         }
         _;
     }
     
-    
-    
-    modifier onlyOwner () {
-        if ( msg.sender != owner ) {
+     modifier onlyDataEntryClerk () {
+        if ( msg.sender != dataentryclerk ) {
             throw;
         }
         _;
@@ -52,12 +112,12 @@ contract DragonLock {
         TimeLock = now + 90 days;
        
         owner = msg.sender;
+        dataentryclerk = msg.sender;
         
     }
     
     
-    
-    
+    //allows token holders to withdar their dragons after timelock expires
     function withdrawDragons(){
         
         require ( now > TimeLock );
@@ -68,16 +128,23 @@ contract DragonLock {
     }
     
     
-    
-    function creditDragon ( address tokenholder, uint amount ) onlyOwner {
+    // manually enter dragon credit to dragon lock  - remember to add 8 extra zeros to compensate for 8 dragon decimals
+    function creditDragon ( address tokenholder, uint amount ) onlyDataEntryClerk {
         
       
-        dragonBalance [ tokenholder ] += amount;
+        dragonBalance [ tokenholder ] = dragonBalance [ tokenholder ].add(amount);
         
     }
     
+    //Used in case data entry clerk makes an error crediting address
+    function resetDragonBalance ( address tokenholder, uint amount ) onlyOwner {
+        
+      
+        dragonBalance [ tokenholder ] = 0;
+        
+    }
    
-    
+    // transfer ownership of this contract
     function transferOwnership ( address _newowner ) onlyOwner {
         
         owner = _newowner;
@@ -85,7 +152,11 @@ contract DragonLock {
     }
     
     
+    function transferDataEntryClerk ( address _dataentryclerk ) onlyOwner {
+        
+        dataentryclerk = _dataentryclerk;
+        
+    }
+    
    
-    
-    
 }
